e06360ad958b1b97b3b85ef6d0178936a073e375
Zahir Alston <zalst0056@launchpadphilly.org>
2026-01-06 10:01:25 -0500

    Fix prisma import for Vercel deployment

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b4189e903eb58239"; }
+/******/ 		__webpack_require__.h = function() { return "0aba5f680ec3cc2f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
